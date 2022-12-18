d8fa16f62766774ef368fb71898ea9ac1bda6186
andylamax <andylamax@programmer.net>
2022-12-18 07:31:42 +0300

    Sunday mornign sync

--- file/src/commonMain/kotlin/epsilon/FileBlobConstructors.kt
+++ file/src/commonMain/kotlin/epsilon/FileBlobConstructors.kt
@@ -1,6 +1,10 @@
 @file:JvmName("FileBlobConstructorsCommon")
+
 package epsilon
 
+import kase.Failure
+import kase.Result
+import kase.Success
 import kotlin.jvm.JvmName
 
 expect inline fun FileBlob(path: String): FileBlob
@@ -9,4 +13,13 @@ inline fun FileBlobOrNull(path: String? = null): FileBlob? = try {
     FileBlob(path = path!!)
 } catch (_: Throwable) {
     null
+}
+
+inline fun FileBlobOrResult(path: String? = null): Result<FileBlob> {
+    if (path == null) return Failure(IllegalArgumentException("Can't obtain a file with a null path param"))
+    return try {
+        Success(FileBlob(path))
+    } catch (err: Throwable) {
+        Failure(err)
+    }
 }
