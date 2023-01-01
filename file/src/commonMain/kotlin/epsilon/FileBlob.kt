bb3da5a7323554082449c93e0fa3e7052adae0d3
andylamax <andylamax@programmer.net>
2023-01-01 04:09:13 +0300

    releasing picortex sdk version 0.01.45

--- file/src/commonMain/kotlin/epsilon/FileBlob.kt
+++ file/src/commonMain/kotlin/epsilon/FileBlob.kt
@@ -2,8 +2,11 @@
 
 package epsilon
 
+import epsilon.serializers.FileBlobSerializer
+import kotlinx.serialization.Serializable
 import kotlin.js.JsExport
 
+@Serializable(with = FileBlobSerializer::class)
 interface FileBlob : Blob {
     val path: String
     val name: String
---
+++ file/src/commonMain/kotlin/epsilon/serializers/FileBlobSerializer.kt
@@ -0,0 +1,37 @@
+package epsilon.serializers
+
+import epsilon.FileBlob
+import epsilon.FileBlobOrNull
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+
+@Serializer(forClass = FileBlob::class)
+object FileBlobSerializer : KSerializer<FileBlob> {
+    override val descriptor = PrimitiveSerialDescriptor(
+        serialName = "epsilon.FileBlob",
+        PrimitiveKind.STRING
+    )
+    private val cache = mutableMapOf<String, FileBlob>()
+
+    override fun deserialize(decoder: Decoder): FileBlob = decoder.decodeString().toFileBlob()
+
+    override fun serialize(encoder: Encoder, value: FileBlob) = encoder.encodeString(value.toCachedSerialPath())
+
+    private fun FileBlob.toCachedSerialPath(): String {
+        val cachedPath = "$name @ $path"
+        cache[cachedPath] = this
+        return cachedPath
+    }
+
+    private fun String.toFileBlob(): FileBlob = cache[this] ?: try {
+        FileBlob(split(" @ ")[1])
+    } catch (err: Throwable) {
+        FileBlobOrNull(this) ?: throw RuntimeException("Failed to resolve $this into a FileBlob")
+    }
+}
