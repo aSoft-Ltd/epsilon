e0f7b2a6794f4886b86f8d43fc42aa394fb531a4
andylamax <andylamax@programmer.net>
2023-06-10 15:08:51 +0300

    Before merging with oss

--- file/src/jsMain/kotlin/epsilon/FileBlobFactory.kt
+++ file/src/jsMain/kotlin/epsilon/FileBlobFactory.kt
@@ -5,7 +5,6 @@ package epsilon
 import epsilon.internal.FileBlobImpl
 import kase.Result
 import kase.catching
-
 @JsName("fileBlobAt")
 actual inline fun FileBlob(path: String?): Result<FileBlob> = catching {
     FileBlobImpl.from(path ?: throw IllegalArgumentException("path must not be null"))
